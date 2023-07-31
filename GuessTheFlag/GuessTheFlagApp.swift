1b9dd2276c26685b38c78e3b44788508f0d4e0e9
Abu Sayeed Roni <roni.abusayeed@dal.ca>
2023-07-31 09:06:54 -0300

    Initial Commit

---
+++ GuessTheFlag/GuessTheFlagApp.swift
@@ -0,0 +1,17 @@
+//
+//  GuessTheFlagApp.swift
+//  GuessTheFlag
+//
+//  Created by Abu Sayeed Roni on 2023-07-31.
+//
+
+import SwiftUI
+
+@main
+struct GuessTheFlagApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
