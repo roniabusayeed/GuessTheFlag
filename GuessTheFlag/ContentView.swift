75abab764db5d8c76930b1666f40fd034832076c
Abu Sayeed Roni <roni.abusayeed@dal.ca>
2023-07-31 21:12:03 -0300

    Enhance UI, limit # of questions and display final score.

--- GuessTheFlag/ContentView.swift
+++ GuessTheFlag/ContentView.swift
@@ -15,54 +15,117 @@ struct ContentView: View {
     @State private var countries = ["Estonia", "France", "Germany", "Ireland", "Italy", "Monaco", "Nigeria", "Poland", "Russia", "Spain", "UK", "US"].shuffled()
     @State private var correctAnswer = Int.random(in: 0..<3)
     
+    @State private var alertMessage: String = ""
+    let correctFeedbackMessages = [
+        "Flag-tastic! Nailed it!",
+        "Whoa, flag whisperer! Amazing guess!",
+        "Flawless flag expertise!",
+        "Bingo! You've got the flag flair!",
+        "Flag wizardry at its finest!",
+        "Hoisting the flag of success! Great job!",
+        "You've cracked the code of flags! Spectacular!",
+        "Flag guru in the making! Impressive guess!",
+        "Unfurling the flag brilliance! Well done!",
+        "Flags bow to your mastery! Fantastic job!"
+    ]
+    
+    let maximumNumberOfQuestions = 8
+    @State private var currentNumberOfQuestion = 0
+    @State private var showingFinalScore: Bool = false
+
+    
     var body: some View {
         ZStack {
-            LinearGradient(colors: [.teal, .black], startPoint: .top, endPoint: .bottom)
-                .ignoresSafeArea()
+            RadialGradient(stops: [.init(color: Color(red: 0.1, green: 0.2, blue: 0.45), location: 0.30), .init(color: Color(red: 0.76, green: 0.15, blue: 0.26), location: 0.30)], center: .top, startRadius: 200, endRadius: 700).ignoresSafeArea()
             
-            VStack(spacing: 30) {
-                VStack {
-                    Text("Tap the flag of")
-                        .foregroundColor(.white)
-                        .font(.subheadline.weight(.heavy))
-                    Text(countries[correctAnswer])
-                        .foregroundColor(.white)
-                        .font(.largeTitle.weight(.semibold))
-                }
+            VStack {
+                Spacer()
+                // Title
+                Text("Guess The Flag")
+                    .font(.title.weight(.bold))
+                    .foregroundColor(.white)
                 
-                ForEach(0..<3) { index in
-                    Button {
-                        flagTapped(index)
-                    } label: {
-                        Image(countries[index])
-                            .renderingMode(.original)
-                            .shadow(radius: 5)
-                            .clipShape(RoundedRectangle(cornerRadius: 18))
+                // Main interactive box.
+                VStack(spacing: 15) {
+                    VStack {
+                        Text("Tap the flag of")
+                            .font(.subheadline.weight(.heavy))
+                            .foregroundStyle(.secondary)    // using style instead of color because of the tranlucency effect it gives. I.e. little bit of background coming through.
+                        Text(countries[correctAnswer])
+                            .font(.largeTitle.weight(.semibold))
+                    }
+                    
+                    ForEach(0..<3) { index in
+                        Button {
+                            flagTapped(index)
+                        } label: {
+                            Image(countries[index])
+                                .renderingMode(.original)
+                                .shadow(radius: 5)
+                                .clipShape(RoundedRectangle(cornerRadius: 20))
+                        }
+                    }
+                    // Immediate feedback alert.
+                    .alert(scoreTitle, isPresented: $showingScore) {
+                        Button("Continue", action: askQuestion)
+                    } message: {
+                        Text(alertMessage)
                     }
                 }
+                .frame(maxWidth: .infinity)
+                .padding(.vertical, 20)
+                .background(.thinMaterial)
+                .clipShape(RoundedRectangle(cornerRadius: 20))
+                .shadow(radius: 5)
                 
+                
+                
+                Spacer()
+                
+                // Display score.
+                Text("Your socre: \(score)")
+                    .foregroundColor(.white)
+                    .font(.title.bold())
+                Spacer()
             }
+            .padding(.horizontal)
         }
-        .alert(scoreTitle, isPresented: $showingScore) {
-            Button("Continue", action: askQuestion)
+        
+        // Final score alert.
+        .alert("Final Score", isPresented: $showingFinalScore) {
+            Button("Restart", action: resetGame)
         } message: {
-            Text("Your score is \(score)")
+            Text("You've scored \(score)/\(maximumNumberOfQuestions)")
         }
     }
     
     func flagTapped(_ index: Int) {
+        currentNumberOfQuestion += 1
+        
         if index == correctAnswer {
             scoreTitle = "Correct"
+            alertMessage = correctFeedbackMessages.randomElement()!
             score += 1
         } else {
-            scoreTitle = "Wrong"
+            scoreTitle = "Wrong!"
+            alertMessage = "That was \(countries[index])."
         }
         showingScore = true // The alert is displayed at this point.
+        
+        // Check end conditions.
+        if currentNumberOfQuestion == maximumNumberOfQuestions {
+            showingFinalScore = true    // The final score alert is displayed at this point.
+        }
     }
     
     func askQuestion() {
         countries.shuffle()
         correctAnswer = Int.random(in: 0..<3)
+    }
+    
+    func resetGame() {
+        score = 0
+        currentNumberOfQuestion = 0
     }
 }
 
